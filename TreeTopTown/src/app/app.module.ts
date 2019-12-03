76ee1023e5b6cddafc2deb6196deec660e4f5885
Daniel Grondin <king@numsgil.co>
2019-12-03 15:23:22 -0500

    Fixed Town app not building

--- TreeTopTown/src/app/app.module.ts
+++ TreeTopTown/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { JqueryComponent } from './jquery/jquery.component';
-import { BootstrapComponent } from './bootstrap/bootstrap.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    JqueryComponent,
-    BootstrapComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
