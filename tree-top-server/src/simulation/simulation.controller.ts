78389f1c548b761951a7715a780dad6831368f9a
Daniel Grondin <king@numsgil.co>
2019-12-04 16:28:54 -0500

    Added Post route to SimulationController

--- tree-top-server/src/simulation/simulation.controller.ts
+++ tree-top-server/src/simulation/simulation.controller.ts
@@ -1,5 +1,5 @@
-import {Controller, Get, Logger, Param} from "@nestjs/common";
-import {SimulationService} from "./simulation.service";
+import {Body, Controller, Get, Logger, Param, Post} from "@nestjs/common";
+import {CreateSimulationDefinition, CreateSimulationDetailDefinition, SimulationService} from "./simulation.service";
 import {Simulation, SimulationDetails} from "./simulation.entity";
 
 @Controller("sim")
@@ -25,5 +25,17 @@ export class SimulationController {
 	async getSimulationDetails(@Param("id") id: number): Promise<SimulationDetails[]> {
 		this.logger.log(`getSimulationDetails with ID = ${id}`);
 		return await this.sims.getDetailsForSimulation(id);
+	}
+
+	@Post()
+	async createSimulation(@Body() data: CreateSimulationDefinition): Promise<Simulation> {
+		this.logger.log(`createSimulation with Budget = ${data.budget}$`);
+		return await this.sims.createSimulation(data);
+	}
+
+	@Post(":id/details")
+	async addDetailsToSimulation(@Param("id") id: number, @Body() data: CreateSimulationDetailDefinition[]): Promise<Simulation> {
+		this.logger.log(`addDetailsToSimulation for Simulation ID = ${id} with ${data.length} details`);
+		return await this.sims.addDetailsToSimulation(id, ...data);
 	}
 }
