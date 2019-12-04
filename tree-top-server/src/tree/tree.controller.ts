9b0f23ec31a0b79b960a2081a1247a127df1750f
Daniel Grondin <king@numsgil.co>
2019-12-04 13:44:02 -0500

    Finished CRUD operations for TreeController

--- tree-top-server/src/tree/tree.controller.ts
+++ tree-top-server/src/tree/tree.controller.ts
@@ -1,5 +1,5 @@
-import {Controller, Get, Logger, Param, Query} from "@nestjs/common";
-import {TreeService} from "./tree.service";
+import {Body, Controller, Delete, Get, Logger, Param, Post, Put, Query} from "@nestjs/common";
+import {CreateTreeDefinition, TreeService} from "./tree.service";
 import {Tree} from "./tree.entity";
 
 @Controller("tree")
@@ -29,5 +29,25 @@ export class TreeController {
 	async getTreeById(@Param("id") id: number): Promise<Tree> {
 		this.logger.log(`getTreeById with id = ${id}`);
 		return await this.trees.getTreeById(id);
+	}
+
+	@Post()
+	async createTree(@Body() data: CreateTreeDefinition): Promise<Tree> {
+		this.logger.log(`createTree with name = '${data.name}'`);
+		return await this.trees.createTree(data);
+	}
+
+	@Put(":id")
+	async updateTree(@Param("id") id: number, @Body() data: CreateTreeDefinition): Promise<Tree> {
+		this.logger.log(`updateTree with id = ${id}`);
+		let tree = await this.trees.getTreeById(id);
+		Object.assign(tree, data);
+		return await this.trees.saveTree(tree);
+	}
+
+	@Delete(":id")
+	async deleteTree(@Param("id") id: number) {
+		this.logger.log(`deleteTree with id = ${id}`);
+		await this.trees.deleteTreeById(id);
 	}
 }
