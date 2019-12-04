aa864d771e9ed1071109c64410183a9131973db3
Daniel Grondin <king@numsgil.co>
2019-12-04 15:28:20 -0500

    Began working on the CRUD operations for Simulations

--- tree-top-server/src/simulation/simulation.controller.ts
+++ tree-top-server/src/simulation/simulation.controller.ts
@@ -1,10 +1,29 @@
-import {Controller, Logger} from "@nestjs/common";
+import {Controller, Get, Logger, Param} from "@nestjs/common";
 import {SimulationService} from "./simulation.service";
+import {Simulation, SimulationDetails} from "./simulation.entity";
 
-@Controller("tree")
+@Controller("sim")
 export class SimulationController {
 	private readonly logger: Logger = new Logger(SimulationController.name);
 
 	constructor(private readonly sims: SimulationService) {
+	}
+
+	@Get()
+	async getAllSimulations(): Promise<Simulation[]> {
+		this.logger.log("getAllSimulations");
+		return await this.sims.getAllSimulations();
+	}
+
+	@Get(":id")
+	async getSimulation(@Param("id") id: number): Promise<Simulation> {
+		this.logger.log(`getSimulation with ID = ${id}`);
+		return await this.sims.getSimulationById(id);
+	}
+
+	@Get(":id/details")
+	async getSimulationDetails(@Param("id") id: number): Promise<SimulationDetails[]> {
+		this.logger.log(`getSimulationDetails with ID = ${id}`);
+		return await this.sims.getDetailsForSimulation(id);
 	}
 }
--- tree-top-server/src/simulation/simulation.service.ts
+++ tree-top-server/src/simulation/simulation.service.ts
@@ -2,6 +2,28 @@ import {Inject, Injectable, Logger} from '@nestjs/common';
 import {Repository} from "typeorm";
 import {Simulation, SimulationDetails} from "./simulation.entity";
 
+export class CreateSimulationDefinition {
+	public readonly budget: number;
+	public readonly terrainSize: number;
+
+	constructor(budget: number, terrainSize: number) {
+		this.budget = budget;
+		this.terrainSize = terrainSize;
+	}
+}
+
+export class CreateSimulationDetailDefinition {
+	public readonly tree: number;
+	public readonly quantity: number;
+	public readonly isBio: boolean;
+
+	constructor(tree: number, quantity: number, isBio: boolean) {
+		this.tree = tree;
+		this.quantity = quantity;
+		this.isBio = isBio;
+	}
+}
+
 @Injectable()
 export class SimulationService {
 	private readonly logger: Logger = new Logger(SimulationService.name);
@@ -12,5 +34,21 @@ export class SimulationService {
 		@Inject("SIMULATION_DETAILS_REPOSITORY")
 		private readonly details: Repository<SimulationDetails>,
 	) {
+	}
+
+	async getAllSimulations(): Promise<Simulation[]> {
+		this.logger.log("Get All Simulations");
+		return await this.sims.find();
+	}
+
+	async getSimulationById(id: number): Promise<Simulation> {
+		this.logger.log(`Get Simulation with ID = ${id}`);
+		return await this.sims.findOneOrFail(id);
+	}
+
+	async getDetailsForSimulation(simId: number): Promise<SimulationDetails[]> {
+		this.logger.log(`Get SimulationDetails for Simulation ${simId}`);
+		let sim = await this.sims.findOneOrFail(simId, {relations: ["details"]});
+		return sim.details;
 	}
 }
