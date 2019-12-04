b06c7b69844a90f54aa38c0f55721f8a122a009f
Daniel Grondin <king@numsgil.co>
2019-12-04 10:42:26 -0500

    Injected TreeService into TreeController

--- tree-top-server/src/tree/tree.controller.ts
+++ tree-top-server/src/tree/tree.controller.ts
@@ -1,9 +1,10 @@
 import {Controller, Logger} from "@nestjs/common";
+import {TreeService} from "./tree.service";
 
 @Controller("tree")
 export class TreeController {
 	private readonly logger: Logger = new Logger(TreeController.name);
 
-	constructor() {
+	constructor(private readonly trees: TreeService) {
 	}
 }
