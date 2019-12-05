3a11d9573f6f1c972dc92443e30657eb8a3a7699
Alexis <exerapidoxthorn@gmail.com>
2019-12-05 11:19:39 -0500

    Template succesfully integrated. Great!

--- tree-top-town/src/app/app.module.ts
+++ tree-top-town/src/app/app.module.ts
@@ -3,17 +3,29 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {ComponentsModule} from "./components/components.module";
+import {RouterModule} from "@angular/router";
+import {ToastrModule} from "ngx-toastr";
+import {AdminLayoutComponent} from "./layouts/admin-layout/admin-layout.component";
+import {AuthLayoutComponent} from "./layouts/auth-layout/auth-layout.component";
 
 @NgModule({
-	declarations: [
-		AppComponent
-	],
 	imports: [
-		BrowserModule,
-		AppRoutingModule
+		BrowserAnimationsModule,
+		FormsModule,
+		HttpClientModule,
+		ComponentsModule,
+		NgbModule,
+		RouterModule,
+		AppRoutingModule,
+		ToastrModule.forRoot()
 	],
+	declarations: [AppComponent, AdminLayoutComponent, AuthLayoutComponent],
 	providers: [],
 	bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule {}
