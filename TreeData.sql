5fd52b404cc14aaea02277bd63e0d0ee6ce59010
Daniel Grondin <king@numsgil.co>
2019-12-05 13:18:06 -0500

    Added SQL script to seed trees (pun intended)

---
+++ TreeData.sql
@@ -0,0 +1,18 @@
+USE TreeTopTown
+GO
+
+INSERT tree (name, maxDiameter, maxProduction, maxAge, mrpPerKg, pesticideUsage, fertilizerUsage)
+VALUES (N'Cocoa', 12, 350, 50, 4, 0, 30),
+       (N'Apple', 10, 200, 70, 1, 20, 10),
+       (N'Plum', 10, 100, 50, 1, 10, 10),
+       (N'Cherry', 10, 110, 50, 1, 10, 10),
+       (N'Cashew', 7, 30, 10, 8, 10, 0),
+       (N'Avocado', 7, 200, 50, 2, 0, 20),
+       (N'Grapefruit', 7, 150, 20, 1.5, 0, 10),
+       (N'Pomegranate', 7, 150, 20, 2, 0, 10),
+       (N'Mango', 10, 150, 20, 2, 0, 10),
+       (N'Kiwi', 4, 150, 30, 1, 0, 10),
+       (N'Banana', 5, 400, 10, 0.5, 0, 10),
+       (N'Lemon', 7, 250, 5, 0.3, 0, 0),
+       (N'Coconut', 13, 1000, 200, 3, 0, 40)
+GO
