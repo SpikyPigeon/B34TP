9cf9e886eeeb57c96ff729d60fc444f9cf873561
Alexis <exerapidoxthorn@gmail.com>
2019-12-05 13:49:35 -0500

    Made TreeTopService work

--- tree-top-town/src/app/treetop.service.ts
+++ tree-top-town/src/app/treetop.service.ts
@@ -5,9 +5,38 @@ import {HttpClient} from "@angular/common/http";
 	providedIn: 'root'
 })
 export class TreeTopService {
+	url: string = 'http://localhost:1337/';
+
 
 	constructor(private readonly http:HttpClient) {
-		
+
 	}
 
+}
+
+export interface Simulation {
+	id:number;
+	createdAt: Date;
+	budget:number;
+	terrainSize:number;
+	details: SimulationDetails[];
+}
+
+export interface Tree{
+	id: number;
+	name: string;
+	maxDiameter: number;
+	maxProduction: number;
+	maxAge: number;
+	mrpPerKg: number;
+	pesticideUsage: number;
+	fertilizerUsage: number;
+}
+
+export interface SimulationDetails{
+	id: number;
+	quantity:number;
+	isBio: boolean;
+	tree: Tree;
+	simulation: Simulation;
 }
